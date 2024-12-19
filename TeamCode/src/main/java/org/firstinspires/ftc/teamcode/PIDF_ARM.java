e73fc07c3faa97dff294313f8e60ce8adb18c7ac
bryaniscool123 <154935728+bryaniscool123@users.noreply.github.com>
2024-12-19 18:45:50 -0500

    beginning implementation of a pidf controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDF_ARM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDF_ARM.java
@@ -1,18 +1,53 @@
-//package org.firstinspires.ftc.teamcode;
-//
-//import com.arcrobotics.dashboard.config.Config;
-//import com.arcrobotics.ftclib.controller.PIDController;
-//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//import com.qualcomm.robotcore.hardware.DcMotorEx;
-//
-//@Config
-//@TeleOp
-//public class PIDF_ARM extends OpMode {
-//    private PIDController controller;
-//    public static double p = 0, i = 0, d= 0;
-//    public static double f=0;
-//
-//    public static int target = 0;
-//    private final double ticks_in_degrees = 700 / 180.0;
-//}
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Config
+@TeleOp
+public class PIDF_ARM extends OpMode {
+    private PIDController controller;
+    public static double p = 0, i = 0, d= 0;
+    public static double f=0;
+
+    public static int target = 0;
+    private final double ticks_in_degrees = 700 / 180.0;
+
+    private DcMotor leftRotate;
+
+    @Override
+    public void init() {
+        controller = new PIDController(p, i, d);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        leftRotate = hardwareMap.get(DcMotor.class, "left Rotate");
+    }
+
+    @Override
+    public void loop() {
+        controller.setPID(p,i,d);
+        int armPos = leftRotate.getCurrentPosition();
+        double pid = controller.calculate(armPos, target);
+        double ff = Math.cos(Math.toRadians(target / ticks_in_degrees)) * f;
+
+        double power = pid + ff;
+
+        leftRotate.setPower(power);
+
+        telemetry.addData("pos: ", armPos);
+        telemetry.addData("target: ", target);
+        telemetry.update();
+    }
+}
+
+//start f at 0.05, and keep on going up until the arm can resist gravity
+//change p to get pos as close to target as possible
+//configure d
+//go to 192.168.43.1:8080/dash
+
+
